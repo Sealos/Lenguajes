33b8f1f16a09e15d44a67282ccff8a45d0bfb2a4
Stefano De Colli <sdecolli@gmail.com>
2014-01-21 20:13:43 -0430

    Come negra y come blanca

--- P2/verificaciones.pl
+++ P2/verificaciones.pl
@@ -4,6 +4,11 @@ blanca('< ').
 
 negra('> ').
 
+negras('> ').
+negras('>>').
+blancas('< ').
+blancas('<<').
+
 ficha(F) :-
 	turno,
 	blanca(F),
@@ -18,10 +23,10 @@ jugada_valida(X1, Y1, X2, Y2) :-
 	jugada_valida_reina(X1,Y1,X2,Y2);
 	jugada_valida_peon(X1,Y1,X2,Y2).
 
-jugada_valida(_, _, _, _) :- !,
-	write('No es una jugada valida.'),
-	nl,
-	fail.
+%jugada_valida(_, _, _, _) :- !,
+%	write('No es una jugada valida.'),
+%	nl,
+%	fail.
 
 jugada_valida_peon(X1,Y1,X2,Y2):-
 	(Y1\=Y2,
@@ -44,3 +49,76 @@ jugada_valida_reina(X1,Y1,X2,Y2) :-
 	ficha(E1),
 	!,
 	E2 = '  '.
+
+
+% X1 posicion X de la ficha
+% Y1 posicion Y de la ficha
+% X2 posicion X de la ficha luego de comer
+% Y2 posicion Y de la ficha luego de comer 
+come_peon(X1, Y1, X2, Y2) :-
+	come_blanca_peon(X1, Y1, X2, Y2);
+	come_negra_peon(X1, Y1, X2, Y2).
+
+% Come hacia arriba (X2 > X1; Y2 > Y1)
+come_negra_peon(X1, Y1, X2, Y2) :-
+	tablero(M),
+	XF is X1 - 1,
+	XF > 1,
+	XFF is XF - 1,
+	(
+		(
+			YF is Y1 +1,
+			YF < 8,
+			get(M, XF, YF, E1),
+			blancas(E1),
+			YFF is YF + 1,
+			get(M, XFF, YFF, E2),
+			E2 = '  ',
+			X2 is XFF,
+			Y2 is YFF
+		)
+		;
+		(
+			YF is Y1 - 1,
+			YF > 1,
+			get(M, XF, YF, E1),
+			blancas(E1),
+			YFF is YF - 1,
+			get(M, XFF, YFF, E2),
+			E2 = '  ',
+			X2 is XFF,
+			Y2 is YFF
+		)
+	).
+
+% Come hacia arriba (X2 > X1; Y2 > Y1)
+come_blanca_peon(X1, Y1, X2, Y2) :-
+	tablero(M),
+	XF is X1 + 1,
+	XF < 8,
+	XFF is XF + 1,
+	(
+		(
+			YF is Y1 +1,
+			YF < 8,
+			get(M, XF, YF, E1),
+			negras(E1),
+			YFF is YF + 1,
+			get(M, XFF, YFF, E2),
+			E2 = '  ',
+			X2 is XFF,
+			Y2 is YFF
+		)
+		;
+		(
+			YF is Y1 - 1,
+			YF > 1,
+			get(M, XF, YF, E1),
+			negras(E1),
+			YFF is YF - 1,
+			get(M, XFF, YFF, E2),
+			E2 = '  ',
+			X2 is XFF,
+			Y2 is YFF
+		)
+	).
