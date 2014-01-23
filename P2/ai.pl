1f20e07fac9d399b98da5a0991ec5d32042c4e96
Stefano De Colli <sdecolli@gmail.com>
2014-01-22 19:47:45 -0430

    AI

--- P2/ai.pl
+++ P2/ai.pl
@@ -2,13 +2,7 @@ obtener_jugadas(Z, LW) :-
 	findall(X, jugada_valida(Z, X), LW).
 
 obtener_jugadas_disponibles(L, S) :-
-	maplist(obtener_jugadas, L, S),
-	maplist(convertir_X_Y, L, P),
-	maplist(convertir_X_Y_L, S, K),
-	write(P),nl,
-	write(K),nl,
-	write(L),nl,
-	write(S),nl.
+	maplist(obtener_jugadas, L, S).
 
 convertir_X_Y_L([], pos(0, 0)).
 
@@ -20,11 +14,43 @@ convertir_X_Y(N, pos(X, Y)) :-
 	X is div(N, 8) + 1,
 	Y is mod(N, 8) + 1.
 
-
+existe_saltos([H|Y]) :-
+	H = [],
+	!,
+	existe_saltos(Y).
+existe_saltos([_|_]) :- !.
 
 jugar_maquina :-
-	maquina,
-	% hacer aqui las jugadas validas
-	!.
+	fichas_jugador_actual(L),
+	saltos_jugador_actual(L, S),
+	obtener_jugadas_disponibles(L, J),
+	jugar_maquina_aux(L, S, J).
 
-jugar_maquina.
+jugar_maquina_aux(L, S, _) :-
+	existe_saltos(S),
+	!,
+	primera_jugada(L, S, Z, W),
+	write('Encontre salto, jugando : '),
+	write(Z),write(' '),write(W),nl.
+	%jugada(Z, W). %hacer predicado
+
+jugar_maquina_aux(L, _, J) :-
+	write('No encontre salto : '),nl,
+	write(L),nl,write(J),nl,
+	primera_jugada(L, J, Z, W),
+	write('Encontre movida, jugando : '),
+	write(Z),write(' '),write(W),nl.
+	%jugada(Z, W). % hacer predicado
+
+primera_jugada([H|_], [X|T], Z, W) :-
+	write(H),nl,
+	write(X),nl,
+	X \= [],
+	!,
+	Z is H,
+	head(X, W).
+
+head([X|_], X).
+
+primera_jugada([_|T], [_|Y], Z, W) :-
+	primera_jugada(T, Y, Z, W).
